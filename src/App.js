f5867640a25127afa7fdfed0668f7917ed05a239
w6fux5 <w6fux5@gmail.com>
2022-06-11 21:56:09 +0800

    router setup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { Directory } from './components';
+import { Home, Navigation } from './routes';
 
-const categories = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womans',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
+const Shop = () => (
+  <>
+    <h1>Shop</h1>
+  </>
+);
+
 export const App = () => {
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import styles from './Navigation.module.scss';
+
+export const Navigation = () => {
+  return (
+    <>
+      <div className={styles.navigation}>
+        <Link to="/" className={styles['logo-container']}>
+          <CrwnLogo />
+        </Link>
+        <div className={styles['nav-links-container']}>
+          <Link to="/shop" className={styles['nav-link']}>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
